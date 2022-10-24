fbc6b60061f5f236fdb1244da1d285cbb10255c5
DaShenJiGo <2662641599@qq.com>
2022-10-24 20:41:17 +0800

    推送项目到github中

---
+++ cloud-config-center-3344/src/main/java/com/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author 小韦
+ * @Date 2022/10/24 20:09
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
