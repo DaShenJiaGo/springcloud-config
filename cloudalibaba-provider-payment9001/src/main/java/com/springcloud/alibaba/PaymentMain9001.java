433287445afdf88d51a6c49b4ef1a972f31b5c2d
DaShenJiGo <2662641599@qq.com>
2022-10-30 13:21:58 +0800

    推送项目到github中

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/springcloud/alibaba/PaymentMain9001.java
@@ -0,0 +1,19 @@
+package com.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author 小韦
+ * @Date 2022/10/30 12:59
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
